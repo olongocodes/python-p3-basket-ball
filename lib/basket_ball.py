1852628d625d255e67907a458bf8e5dcf3e467fa
Kevin <kevinolongo9@gmail.com>
2023-11-28 11:47:54 +0300

    Iterate through a dictionary

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -182,3 +182,172 @@ def game_dict():
             ]
         }
     }
+
+
+def num_points_per_game(player_name):
+    #Accessing the game dictionary
+    game_data = game_dict()
+
+    #Searching for the player in the home team
+    for player in game_data["home"]["players"]:
+        if player["name"] == player_name:
+            return player["points_per_game"]
+        
+    #searching for the player in away team  
+    for player in game_data["away"]["players"]:
+        if player["name"] == player_name:
+            return player["points_per_game"]
+    
+    #return None if the player is not found
+    return None
+
+# Example usage:
+# player_name = "Bradley Beal"
+# points_per_game = num_points_per_game(player_name)
+
+# if points_per_game is not None:
+#     print(f"{player_name} has an average of {points_per_game} points per game.")
+# else:
+#     print(f"Player {player_name} not found.")
+
+
+def player_age(player_name):
+    #Accesiing the game dictionary
+    game_data = game_dict()
+
+    #Searching fo player in the home team
+    for player in game_data["home"]["players"]:
+        if player["name"] == player_name:
+            return player["age"]
+    
+    #searching for player in the away team
+    for player in game_data["away"]["players"]:
+        if player["name"] == player_name:
+            return player["age"]
+        
+    # Return None if the player is not found
+    return None
+
+# Example usage:
+# player_name = "Bradley Beal"
+# player_age_result = player_age(player_name)
+
+# if player_age_result is not None:
+#     print(f"{player_name}'s age is {player_age_result}.")
+# else:
+#     print(f"Player {player_name} not found.")  
+
+def team_colors(team_name):
+    #Accessing the game dictionary
+    game_data = game_dict()
+
+    #Checking if the team is in the home team
+    if team_name == game_data["home"]["team_name"]:
+        return game_data["home"]["colors"]
+    
+    #checking if the team is the away team
+    elif team_name == game_data["away"]["team_name"]:
+        return game_data["away"]["colors"]
+    
+    # Return None if the team is not found
+    return None
+    
+
+
+# Example usage:
+# team_name = "Washington Wizards"
+# team_colors_result = team_colors(team_name)
+
+# if team_colors_result is not None:
+#     print(f"The colors of {team_name} are: {', '.join(team_colors_result)}.")
+# else:
+#     print(f"Team {team_name} not found.")
+
+def team_names():
+    game_data = game_dict()
+
+    #Extract team names from the dictionary
+    home_team_name = game_data["home"]["team_name"]
+    away_team_name = game_data["away"]["team_name"]
+
+    #Return a list of team names
+    return [home_team_name, away_team_name] 
+
+# Example usage:
+# teams = team_names()
+# print(f"The teams in the game are: {', '.join(teams)}.")
+
+def player_numbers(team_name):
+    game_data = game_dict()
+
+    #checking if the team is the home team
+    if team_name == game_data["home"]["team_name"]:
+        return[player["number"] for player in game_data["home"]["players"]]
+    
+    #checking if the team is the away team
+    elif team_name == game_data["away"]["team_name"]:
+        return [player["number"] for player in game_data["away"]["players"]]
+    
+    return None
+
+# Example usage:
+# team_name = "Cleveland Cavaliers"
+# player_numbers_result = player_numbers(team_name)
+
+# if player_numbers_result is not None:
+#     print(f"The jersey numbers for {team_name} are: {', '.join(map(str, player_numbers_result))}.")
+# else:
+#     print(f"Team {team_name} not found.")
+
+def player_stats(player_name):
+    game_data = game_dict()
+
+    #searching for the player in the home team
+    for player in game_data["home"]["players"]:
+        if player["name"] == player_name:
+            return player
+        
+    #searching for player in the away team
+    for player in game_data["away"]["players"]:
+        if player["name"] == player_name:
+            return player
+    
+    return None
+
+# Example usage:
+# player_name = "Rui Hachimura"
+# player_stats_result = player_stats(player_name)
+
+# if player_stats_result is not None:
+#     print(f"The stats for {player_name} are:\n{player_stats_result}")
+# else:
+#     print(f"Player {player_name} not found.")
+
+def average_rebounds_by_shoe_brand():
+    # Accessing the game dictionary
+    game_data = game_dict()
+
+    # Create a dictionary to store shoe brands and corresponding rebounds
+    shoe_brand_rebounds = {}
+
+    # Populate the shoe_brand_rebounds dictionary
+    for team in game_data.values():
+        for player in team["players"]:
+            shoe_brand = player["shoe_brand"]
+            rebounds = player["rebounds_per_game"]
+
+            # Check if the shoe brand is already in the dictionary
+            if shoe_brand in shoe_brand_rebounds:
+                shoe_brand_rebounds[shoe_brand].append(rebounds)
+            else:
+                shoe_brand_rebounds[shoe_brand] = [rebounds]
+
+    # Calculate and print the average rebounds for each shoe brand
+    for brand, rebounds_list in shoe_brand_rebounds.items():
+        average_rebounds = sum(rebounds_list) / len(rebounds_list)
+        print(f"{brand}:  {average_rebounds:.2f} rebounds")
+
+# Example usage:
+average_rebounds_by_shoe_brand()
+
+    
